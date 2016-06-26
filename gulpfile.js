848f51eee26bcb8fad43930ae51f414ea08593b6
urayandra <uray91@gmail.com>
2016-06-26 21:57:49 +0700

    stash test

--- gulpfile.js
+++ gulpfile.js
@@ -99,8 +99,8 @@ gulp.task('env', function(){
     .pipe(gulp.dest(path))
 });
 
-gulp.task('default', ['sass', 'js', 'browserSync'], function() {
-    gulp.watch('*.scss', {cwd: path + '/sass'}, ['sass']);
-    gulp.watch('**/*.scss', {cwd: path + '/sass'}, ['sass']);
-    gulp.watch('*.js', {cwd: path + '/js'}, ['js']);
-});
+// gulp.task('default', ['sass', 'js', 'browserSync'], function() {
+//     gulp.watch('*.scss', {cwd: path + '/sass'}, ['sass']);
+//     gulp.watch('**/*.scss', {cwd: path + '/sass'}, ['sass']);
+//     gulp.watch('*.js', {cwd: path + '/js'}, ['js']);
+// });
