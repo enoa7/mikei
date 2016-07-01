4080942a9b391d6ebc7f27ed67b8ec527a0c5301
enoa7 <gamal.ardi@gmail.com>
2016-07-01 09:00:29 +0700

    rename home to aboutus, set featured image to fullwidth with mainBanner_lg img size

--- backend/wp-content/themes/mikei/page-aboutus.php
+++ backend/wp-content/themes/mikei/page-aboutus.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area frontpage">
 		<main id="main" class="site-main" role="main">
 		<div class="banner main-banner">
-			<?php the_post_thumbnail(array('class' => 'img-responsive')); ?>
+			<?php the_post_thumbnail('mainBanner_lg', array('class' => 'img-responsive fullwidth')); ?>
 		</div>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<?php the_content(); ?>
