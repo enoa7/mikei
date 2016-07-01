785224e92bc903cdca89b7d79162513a461c3d24
gamal <gamal.pratama@scm.co.id>
2016-07-01 11:42:27 +0700

    set about us content as posts

--- backend/wp-content/themes/mikei/page-aboutus.php
+++ backend/wp-content/themes/mikei/page-aboutus.php
@@ -9,16 +9,12 @@ get_header(); ?>
 
 	<div id="primary" class="content-area frontpage">
 		<main id="main" class="site-main" role="main">
-		<div class="banner main-banner">
-			<?php the_post_thumbnail('mainBanner_lg', array('class' => 'img-responsive fullwidth')); ?>
-		</div>
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php 
-				the_content();
-				echo 'test';
-			?>
-		<?php endwhile; endif; ?>
-
+			<div class="banner main-banner">
+				<?php the_post_thumbnail('mainBanner_lg', array('class' => 'img-responsive fullwidth')); ?>
+			</div>
+			<div class="user-content">
+				
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
