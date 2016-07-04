0502f343d3fa4d384a2a212607df06cc83fd9e73
gamal <gamal.pratama@scm.co.id>
2016-07-04 16:57:15 +0700

    Contact Us Page
    - download wpforms to generate our form easily
    - add google map (to edit this you have to edit this thru the backend)
    - add width 100%, height 600px and pointer events none on the gmap
    - forms done (frontend) but still not working yet

--- backend/wp-content/themes/mikei/page-aboutus.php
+++ backend/wp-content/themes/mikei/page-aboutus.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area frontpage aboutus">
+	<div id="primary" class="content-area aboutus">
 		<main id="main" class="site-main" role="main">
 			<div class="banner main-banner">
 				<?php the_post_thumbnail('mainBanner_lg', array('class' => 'img-responsive fullwidth')); ?>
